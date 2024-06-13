771bbba09c9982b989f77c99b80da102acbc27ac
WanFengyyds <1041620224kai@gmail.com>
2024-06-13 16:08:05 +0200

    Update with Ai integration

---
+++ WebApplication1/MessageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1
+{
+    public class MessageModel
+    {
+        public string messaggio { get; set; }
+    }
+
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OllamaSharp;
 using WebApplication1;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
@@ -34,12 +36,20 @@ var configValue = builder.Configuration["ConnectionStrings:WebApiDatabase"];
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(configValue));
 
 var app = builder.Build();
+// set up the client
 
-using (var scope = app.Services.CreateScope())
+var uri = new Uri("http://localhost:11434");
+var ollama = new OllamaApiClient(uri)
+{
+    // select a model which should be used for further operations
+    SelectedModel = "phi3"
+};
+
+/*using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     db.Database.Migrate();
-}
+}*/
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
@@ -76,9 +86,35 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+app.MapPost("/ollama", async ([FromBody] MessageModel model) =>
+{
+    var entireMessage = "";
+    var chat = ollama.Chat(stream =>
+    {
+        if (!stream.Done)
+        {
+            entireMessage += stream.Message?.Content ?? "";
+        }
+    });
+    await chat.Send(model.messaggio);
+    return entireMessage;
+})
+
+    .RequireAuthorization()
+.WithName("GetOllama")
+.WithOpenApi();
+
+
+
+
+
 app.Run();
+
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+
+
